e70476ff0d78706b529cd7d44c8e2d8dcde4c2cd
Mayukh <mayukhchatterjee2023@gmail.com>
2023-06-20 15:22:22 +0530

    Delete Functionality

--- backend/index.js
+++ backend/index.js
@@ -4,20 +4,15 @@ const product= require('./models/product')
 const cors=require('cors');
 const bodyParser = require('body-parser');
 
-
-
-
-
 const app=Express();
 // Add this line below your other imports
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/getData', (req, res) => {
+app.post('/getData', (req, res) => {      //FOR POSTING
     const { name, email, phone } = req.body;
-  
-    // Create a new product record in the MySQL table
+   // Create a new product record in the MySQL table
     product.create({ name, email, phone })
       .then((createdProduct) => {
         console.log(createdProduct);
@@ -30,9 +25,33 @@ app.post('/getData', (req, res) => {
   });
   
 
-// app.listen(3000,()=>{
-//     console.log("Server Running")
-// })
+  app.get('/getData', (req, res) => {        //FOR FETCHING
+    // Fetch the data from the MySQL table
+    product.findAll()
+      .then((data) => {
+        res.status(200).json(data); // Send the fetched data as a response
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+      });
+  });
+
+  app.delete('/getData/:id', (req, res) => {   //FOR DELETING
+    const { id } = req.params;
+  
+    // Delete the record from the MySQL table
+    product.destroy({ where: { id } })
+      .then(() => {
+        res.status(200).json({ message: 'Item deleted successfully' });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+      });
+  });
+  
+  
 
 product.sync()
 .then((res)=>{
