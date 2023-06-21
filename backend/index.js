e0028363a02b91e4d8921ebd5f7d1be48f3d9822
Mayukh <mayukhchatterjee2023@gmail.com>
2023-06-21 22:51:22 +0530

    JWT

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const Express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const sequelize = require('./database/database');
 const bookingController = require('./controller/bookingcontroller');
 const cors = require('cors');
@@ -10,7 +12,7 @@ app.use(cors());
 app.use(Express.json());
 
 app.use((req,res,next)=>{
-  User.findByPk(1)
+  User.findByPk(2)
   .then(user=>{
     req.user=user;
     next()
@@ -23,6 +25,90 @@ app.post('/getData', bookingController.createProduct);
 app.put('/getData/:id', bookingController.updateProduct);
 app.delete('/getData/:id', bookingController.deleteProduct);
 
+//***For SignUp */
+
+// *** For SignUp ***
+app.post('/signup', (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Check if the email already exists in the database
+  User.findOne({ where: { email } })
+    .then((existingUser) => {
+      if (existingUser) {
+        return res.status(400).json({ error: 'Email already exists' });
+      }
+
+      // Hash the password
+      bcrypt.hash(password, 10)
+        .then((hashedPassword) => {
+          // Create a new user with the hashed password
+          User.create({ name, email, password: hashedPassword })
+            .then((newUser) => {
+              // Generate a JWT token for the new user
+              const token = jwt.sign(
+                { userId: newUser.id, name: newUser.name },
+                'your-secret-key',
+                { expiresIn: '1h' }
+              );
+
+              res.json({ token });
+            })
+            .catch((error) => {
+              console.error(error);
+              res.status(500).json({ error: 'Failed to create user' });
+            });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({ error: 'Internal server error' });
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+});
+
+//*** Login ***
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  
+  // Find the user in the database (using your Sequelize model)
+  User.findOne({ where: { email } })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid email' });
+      }
+
+      // Compare the provided password with the hashed password
+      bcrypt.compare(password, user.password)
+        .then((result) => {
+          if (!result) {
+            return res.status(401).json({ error: 'Invalid password' });
+          }
+
+          // Generate a JWT token
+          const token = jwt.sign(
+            { userId: user.id, name: user.name },
+            'your-secret-key',
+            {
+              expiresIn: '1h', // Token expiration time
+            }
+          );
+
+          // Return the token in the response
+          res.json({ token });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({ error: 'Internal server error' });
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+});
 User.hasMany(Product);
 Product.belongsTo(User)
 
@@ -31,12 +117,6 @@ sequelize
   .then((result) => {
     console.log('Database synced');
         return User.findByPk(1);
-  })
-  .then((user)=>{
-    if(!user){
-      return User.create({name:'Mayukh',email:'mkc360@gmail.com'})
-    }
-    return user;
   })
   .then(user=>{
     // console.log(user);
--- backend/models/user.js
+++ backend/models/user.js
@@ -2,15 +2,19 @@ const Sequelize=require("sequelize")
 const sequelize= require("../database/database")
 
 const User= sequelize.define('user',{
-    id:{
-        type:Sequelize.INTEGER,
-        autoIncrement:true,
-        allowNll:false,
-        primaryKey:true
-
-    },
-    name:Sequelize.STRING,
-    email:Sequelize.STRING
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING, // or DataTypes.TEXT if you expect longer passwords
+        allowNull: false,
+      },
 })
 
 module.exports= User;
